b07ded9ab155516ac5e4b6a655c468f4ad504049
kuruba.bajarappa <kuruba.bajarappa@codenetix.com>
2023-10-21 23:38:50 +0530

    Added add album component

---
+++ src/components/AddAlbum.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+
+export default function AddAlbum({ addAlbum, onOpenDialog }) {
+  const [newAlbumTitle, setNewAlbumTitle] = useState("");
+  const [newUserId, setNewUserId] = useState("");
+
+  const handleAddAlbum = () => {
+    if (newAlbumTitle.trim() === "" || newUserId.trim() === "") {
+      return alert("Album title and user Id can't be empty");
+    }
+    addAlbum({ title: newAlbumTitle, userId: newUserId });
+  };
+
+  return (
+    <>
+      <div
+        className="relative z-10"
+        aria-labelledby="modal-title"
+        role="dialog"
+        aria-modal="true"
+      >
+        {/* <!--
+    Background backdrop, show/hide based on modal state.
+
+    Entering: "ease-out duration-300"
+      From: "opacity-0"
+      To: "opacity-100"
+    Leaving: "ease-in duration-200"
+      From: "opacity-100"
+      To: "opacity-0"
+  --> */}
+        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            {/* <!--
+        Modal panel, show/hide based on modal state.
+
+        Entering: "ease-out duration-300"
+          From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          To: "opacity-100 translate-y-0 sm:scale-100"
+        Leaving: "ease-in duration-200"
+          From: "opacity-100 translate-y-0 sm:scale-100"
+          To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+      --> */}
+            <div className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+              <div>
+                {" "}
+                <div>
+                  <label
+                    htmlFor="userId"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    User ID
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      value={newUserId}
+                      onChange={(e) => setNewUserId(e.target.value)}
+                      type="text"
+                      name="userId"
+                      id="userId"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      placeholder="Userid"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <label
+                    htmlFor="albumTitle"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Album tilte
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      value={newAlbumTitle}
+                      onChange={(e) => setNewAlbumTitle(e.target.value)}
+                      type="text"
+                      name="albumTitle"
+                      id="albumTitle"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      placeholder="Album title"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+                <button
+                  onClick={handleAddAlbum}
+                  type="button"
+                  className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2"
+                >
+                  Add Album
+                </button>
+                <button
+                  onClick={onOpenDialog}
+                  type="button"
+                  className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
