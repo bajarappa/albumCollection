23bdca9e298d6681d38d575b61cc8281bdc5cf0d
kuruba.bajarappa <kuruba.bajarappa@codenetix.com>
2023-10-21 23:39:18 +0530

    added header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+export default function Header({ onOpenDialog }) {
+  return (
+    <>
+      <nav className="bg-gray-100 p-4 mx-auto max-w-7xl sm:my-4 sm:rounded-lg  ">
+        <div className="flex justify-between items-center">
+          <h1 className="text-3xl font-bold">Album Collection</h1>
+          <button
+            onClick={onOpenDialog}
+            type="button"
+            className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Add album
+          </button>
+        </div>
+      </nav>
+    </>
+  );
+}
